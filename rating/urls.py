bdeddf8a54848125a1e4fd782d879558ba18ef6a
HasibKUCSE <hasibsdvrt555@gmail.com>
2019-01-19 01:02:11 +0600

    adding templates inde, product, shop

--- rating/urls.py
+++ rating/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:mall_id>/mall/', views.shop, name='shop'),
+    path('<int:shop_id>/shop/', views.product, name='product'),
 ]
--- rating/views.py
+++ rating/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.template import loader
+from django.http import HttpResponse, Http404
+from .models import Mall, Shop, Product
 
 
 def index(request):
-    return HttpResponse("Hello")
+    mall_list = Mall.objects.all()
+    #template = loader.get_template('rating/index.html')
+    context ={
+        'mall_list': mall_list,
+    }
+    return render(request, 'rating/index.html', context)
+
+
+def shop(request, mall_id):
+    try:
+        mall = Mall.objects.get(pk=mall_id)
+    except Mall.DoesNotExist:
+        raise Http404("Mall does not exit")
+    return render(request, 'rating/shop.html', {'mall': mall})
+
+
+def product(request, shop_id):
+    try:
+        shopp = Shop.objects.get(pk=shop_id)
+    except Shop.DoesNotExist:
+        raise Http404("Shop does not exit")
+    return render(request, 'rating/product.html', {'shopp': shopp})
 
 
